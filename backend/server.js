c53ba83f0405d900c244899e168bbb2740683f51
ouadie <ouadieffantouz@gmail.com>
2025-12-16 17:55:55 +0100

    start on backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config()
+const cors = require('cors');
+const PORT = process.env.PORT || 3000
+const connectDb = require('./Config/ConnectionDb')
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    connectDb(),
+    console.log(`http://localhost:${PORT}`)
+})
